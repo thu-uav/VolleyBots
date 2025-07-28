5079b65291dabf1d6857c052b7d3c1e03c9f5001
zhangruize <2290321870@qq.com>
2025-07-29 01:20:38 +0800

    feat: add hover task

---
+++ scripts/training_shell/hover.sh
@@ -0,0 +1,19 @@
+total_frames=500_000_000
+algorithm="mappo"  # mappo, maddpg, td3, sac, dqn
+action_transform=""  # PIDrate, null
+seed=0  # 0, 1, 2
+
+CUDA_VISIBLE_DEVICES=0 python ../train.py headless=true \
+    total_frames=${total_frames} \
+    task=Hover \
+    task.drone_model=Iris \
+    task.env.num_envs=4096 \
+    viewer.eye="[16., 0., 8.]" \
+    viewer.lookat="[0., 0., 0.]" \
+    eval_interval=50 \
+    save_interval=500 \
+    algo=${algorithm} \
+    task.time_encoding=false \
+    task.action_transform=${action_transform}\
+    seed=${seed} \
+    wandb.mode=disabled \
